c9818f374b03b3603d46430889e2bb702dd98f3b
unknown <sergio.jimenez@uabc.edu.mx>
2018-07-20 12:54:32 -0700

    The catalog is now fucntional and displays the correct information for each nutritionist,thoug it still needs to be polish to make it attractive

--- client/app/components/Home/CatalogueNutriologist.js
+++ client/app/components/Home/CatalogueNutriologist.js
@@ -1,5 +1,16 @@
 import React, { Component,Children } from 'react';
-
+import Modal from 'react-modal';
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    backgroundColor       :'#98fb98'
+  }
+}
 class CatalogueNutriologist extends Component {
  
   constructor() {
@@ -8,10 +19,22 @@ class CatalogueNutriologist extends Component {
 
     this.state = {
       Nutritionists:[],
+      isActive:null,
+      activeModal:null
+
     
     };
+    this.clickHandler =this.clickHandler.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+    //this.toggleModal = this.toggleModal.bind(this);
 
   }
+  clickHandler(e, index) {
+    this.setState({ activeModal: index })
+}
+hideModal() {
+  this.setState({ activeModal: null })
+}
 
   componentDidMount(){
   fetch('/api/accounts/nutritionistcatalog', {method:'GET'})
@@ -26,27 +49,46 @@ class CatalogueNutriologist extends Component {
       
     });
   }
+  toggleModal()
+  {
+    this.setState({
+      isActive:!this.state.isActive
+    });
+
+  }
  
 render()
 {
   //This is used to convert the Nutritionist constant from a JSON to an Array
   var Nutritionists = Array.from(this.state.Nutritionists);
-  console.log(this.state.Nutritionists);
- 
+  //console.log(this.toggleModal);
+ var that=this;
+ console.log(that.clickHandler);
   return(
     <div>
       <h1>Nutriologos</h1>
-      { //using the variable Nutritionist we map the values
-        Nutritionists.map(function(nutritionist){
+      { //using the variable Nutritionist we map the values 
+       Nutritionists.map(function(nutritionist,index){
         return(
             // the values are now in nutritionis and the names of the fields
             //  are the same ast the ones in the JSON
           <div key={nutritionist._id} className="nutritionist">
-            <a href={nutritionist._id}>
+            <a >  
+            <button  id={nutritionist} onClick={e => that.clickHandler(e,index)}>abrir</button>
                     {nutritionist.FirstName}
-                    is looking for a 
-                    {nutritionist.LastName}
-                    {nutritionist.Role}
+                    <Modal id= {nutritionist} isOpen={that.state.activeModal == index} onRequestClose={that.hideModal} style={customStyles}>
+                    
+                    {nutritionist.LastName}<br />
+                    {nutritionist.Role}<br />
+                    {nutritionist.LastName}<br />
+                    {nutritionist.Role}<br />
+                    {nutritionist.LastName}<br />
+                    {nutritionist.Role}<br />
+                    {nutritionist.LastName}<br />
+                    {nutritionist.Role}<br />
+                    </Modal>
+                    {/* <button  onClick={that.toggleModal}>abrir</button> */}
+
             </a>
             </div>
             )
