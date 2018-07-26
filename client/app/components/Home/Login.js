ad012b286c01dd8ca07b78a3604e391514a30bdd
carlohgameros <david.alfredo.alvarez.morales@uabc.edu.mx>
2018-07-26 13:08:33 -0700

    recreation of the login component

--- client/app/components/Home/Login.js
+++ client/app/components/Home/Login.js
@@ -206,44 +206,118 @@ class Login extends Component {
       return (<div><p>Loading...</p></div>);
     }
 
-    if (!token) {
-      return (
-        <div>
-          <div>
-            {
-              (loginError) ? (
-                <p>{loginError}</p>
-              ) : (null)
-            }
-            <h1>Log In</h1>
-            <input
-              name="loginEmail"
-              type="text"
-              placeholder="Email"
-              value={loginEmail}
-              onChange={this.handleInputChange}
-            />
-            <br />
-            <input
-              type="password"
-              name="loginPassword"
-              placeholder="Password"
-              value={loginPassword}
-              onChange={this.handleInputChange}
-            />
-            <br />
-            <button type="button" className="btn btn-dark" onClick={this.onLogin}>Log In</button>
-          </div>
-        </div>
-      );
-    }
+    // if (!token) {
+    //   return (
+        
+        
+
+
+
+
+
+        
+    //   );
+    // }
 
     return (
       <div>
-      </div>
-        
+        <section className="login-block">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4 login-sec">
+                <h2 className="text-center">Login Now</h2>
+                <form className="login-form">
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1" className="text-uppercase">Username</label>
+                    <input type="text" className="form-control" placeholder=""/>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="exampleInputPassword1" className="text-uppercase">Password</label>
+                    <input type="password" className="form-control" placeholder=""/>
+                  </div>
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input type="checkbox" className="form-check-input"/>
+                      <small>Remember Me</small>
+                    </label>
+                    <button type="submit" className="btn btn-login float-right">Submit</button>
+                  </div>
+                </form>
+              </div>
+              <div className="col-md-8 banner-sec">
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                  <ol className="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <div className="carousel-inner" role="listbox">
+                    <div className="carousel-item active">
+                    <img className="d-block img-fluid" width="1100px" height="500px" src="/assets/img/img 1.png" alt="First slide"/>
+                      <div className="carousel-caption d-none d-md-block">
+                        <div className="banner-text">
+                            <h2>Solutions for helth</h2>
+                            <h5 style={{color: #fff}}>In Vielyf we have the determination to create the best software for you and your needs</h5>
+                        </div>	
+                      </div>
+                    </div>
+                    {/* <div className="carousel-item">
+                    <img className="d-block img-fluid" width="1100px" height="500px" src="https://images.pexels.com/photos/1028599/pexels-photo-1028599.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Second slide"/>
+                      <div className="carousel-caption d-none d-md-block">
+                        <div className="banner-text">
+                            <h2>This is Heaven</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                        </div>	
+                      </div>
+                    </div>
+                    <div className="carousel-item">
+                    <img className="d-block img-fluid" width="1100px" height="500px" src="https://images.pexels.com/photos/204686/pexels-photo-204686.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Third slide"/>
+                      <div className="carousel-caption d-none d-md-block">
+                        <div className="banner-text">
+                            <h2>This is Heaven</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                        </div>	
+                      </div>
+                    </div> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section> 
+      </div> 
     );
   }
 }
 
 export default Login;
+
+// {document.write(new Date().getFullYear())}
+
+// <div>
+        //   <div>
+        //     {
+        //       (loginError) ? (
+        //         <p>{loginError}</p>
+        //       ) : (null)
+        //     }
+        //     <h1>Log In</h1>
+        //     <input
+        //       name="loginEmail"
+        //       type="text"
+        //       placeholder="Email"
+        //       value={loginEmail}
+        //       onChange={this.handleInputChange}
+        //     />
+        //     <br />
+        //     <input
+        //       type="password"
+        //       name="loginPassword"
+        //       placeholder="Password"
+        //       value={loginPassword}
+        //       onChange={this.handleInputChange}
+        //     />
+        //     <br />
+        //     <button type="button" className="btn btn-dark" onClick={this.onLogin}>Log In</button>
+        //   </div>
+        // </div>
