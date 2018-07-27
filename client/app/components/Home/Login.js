19c5bea170ff3e3cec1c22d736e824b879791767
Jose Carlos Bolaños Marquez <josex670@gmail.com>
2018-07-27 12:38:00 -0700

    unified views

--- client/app/components/Home/Login.js
+++ client/app/components/Home/Login.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
-import 'whatwg-fetch';
-import { Link } from 'react-router-dom';
-var vista = ('');
-import {
-  getFromStorage,
-  setInStorage,
-} from '../../utils/storage';
+import React, { Component } from "react";
+import "whatwg-fetch";
+import { Link } from "react-router-dom";
+var vista = "";
+import { getFromStorage, setInStorage } from "../../utils/storage";
 
 class Login extends Component {
   constructor(props) {
@@ -13,15 +10,15 @@ class Login extends Component {
 
     this.state = {
       isLoading: true,
-      token: '',
-      signUpError: '',
-      loginError: '',
-      loginEmail: '',
-      loginPassword: '',
-      signUpEmail: '',
-      signUpPassword: '',
-      signUpFirstName: '',
-      signUpLastName: ''
+      token: "",
+      signUpError: "",
+      loginError: "",
+      loginEmail: "",
+      loginPassword: "",
+      signUpEmail: "",
+      signUpPassword: "",
+      signUpFirstName: "",
+      signUpLastName: ""
     };
 
     this.onLogin = this.onLogin.bind(this);
@@ -30,7 +27,7 @@ class Login extends Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
   }
-  
+
   handleInputChange(event) {
     const target = event.target;
     const value = target.value;
@@ -42,10 +39,10 @@ class Login extends Component {
   }
 
   componentDidMount() {
-    const obj = getFromStorage('the_main_app');
+    const obj = getFromStorage("the_main_app");
     if (obj && obj.token) {
       const { token } = obj;
-      fetch('/api/account/verify?token=' + token)
+      fetch("/api/account/verify?token=" + token)
         .then(res => res.json())
         .then(json => {
           if (json.success) {
@@ -53,142 +50,173 @@ class Login extends Component {
               token,
               isLoading: false
             });
-            if (localStorage.hasOwnProperty('email')) {
-              fetch('/api/account/isnutriologist?token='+localStorage.getItem('email'))
-              .then(res => res.json())
-              .then(isnutriologit => {
-                if(isnutriologit.success){
-                  window.location=('/vistanutriologo');
-                } else {
-                  window.location=('/vistacliente');
-                }
-              });
+            if (localStorage.hasOwnProperty("email")) {
+              fetch(
+                "/api/account/isnutriologist?token=" +
+                  localStorage.getItem("email")
+              )
+                .then(res => res.json())
+                .then(isnutriologit => {
+                  if (isnutriologit.success) {
+                    //                    setInStorage("userrole", { token: "Nutriologist" });
+
+                    window.location = "/vistacliente";
+                    //window.location=('/vistanutriologo');
+                  } else {
+                    //window.location = "/vistaprincipal";
+                    //                  setInStorage("userrole", { token: "Client" });
+
+                    window.location = "/vistacliente";
+                  }
+                });
             } else {
               this.setState({
-                isLoading: false,
+                isLoading: false
               });
-            }  
+            }
           }
-      });
+        });
     } else {
       this.setState({
-        isLoading: false,
+        isLoading: false
       });
     }
   }
 
   onLogin() {
-
-    const {
-      loginEmail,
-      loginPassword,
-    } = this.state;
+    const { loginEmail, loginPassword } = this.state;
 
     this.setState({
-      isLoading: true,
+      isLoading: true
     });
-    
-    fetch('/api/account/login', {
-      method: 'POST',
+
+    fetch("/api/account/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({        
+      body: JSON.stringify({
         Email: loginEmail,
-        Password: loginPassword,
-      }),
-    }).then(res => res.json())
+        Password: loginPassword
+      })
+    })
+      .then(res => res.json())
       .then(json => {
-        localStorage.setItem('email', json.Email)
+        localStorage.setItem("email", json.Email);
         if (json.success) {
-          setInStorage('the_main_app', { token: json.token });
+          setInStorage("the_main_app", { token: json.token });
           this.setState({
             loginError: json.message,
             isLoading: false,
-            loginPassword: '',
-            token: json.token,
+            loginPassword: "",
+            token: json.token
           });
-          fetch('/api/account/isnutriologist?token='+ loginEmail)
+          fetch("/api/account/isnutriologist?token=" + loginEmail)
             .then(res => res.json())
             .then(json1 => {
-              if(json1.success){
-                localStorage.setItem('Auth', loginEmail)
-                window.location=('/vistanutriologo');
-                localStorage.setItem('Rol', 'Nutriologo'); 
+              if (json1.success) {
+                localStorage.setItem("Auth", loginEmail);
+                window.location = "/vistacliente";
+                //  window.location = "/vistanutriologo";
+                localStorage.setItem("Rol", "Nutriologo");
               } else {
                 var getuser;
-                fetch('/api/account/getuseremail?token='+loginEmail)
-                .then(res => res.json())
-                .then(json2 => {
-                  fetch('/api/accounts/getuser?token='+json2[0]._id)
+                fetch("/api/account/getuseremail?token=" + loginEmail)
                   .then(res => res.json())
-                  .then(json3 => {
-                    fetch('/api/account/getuserbyid?token='+json3.Nutritionist_id)
-                    .then(res => res.json())
-                    .then(json4 => {
-                      localStorage.setItem('AssignedNutriologist', json4[0].Email)
-                    })
-                  })       
-                })
-                localStorage.setItem('Rol', 'Cliente');  
-                window.location=('/vistacliente');
+                  .then(json2 => {
+                    fetch("/api/accounts/getuser?token=" + json2[0]._id)
+                      .then(res => res.json())
+                      .then(json3 => {
+                        fetch(
+                          "/api/account/getuserbyid?token=" +
+                            json3.Nutritionist_id
+                        )
+                          .then(res => res.json())
+                          .then(json4 => {
+                            console.log("hola");
+                            console.log(json4);
+                            localStorage.setItem(
+                              "AssignedNutriologist",
+                              json4[0].Email
+                            );
+                          });
+                      });
+                  });
+                localStorage.setItem("Rol", "Cliente");
+                //    window.localtion = "/vistaprincipal";
+                window.location = "/vistacliente";
               }
-            });    
+            });
         } else {
           this.setState({
             loginError: json.message,
-            isLoading: false,
+            isLoading: false
           });
         }
       });
-      this.setState({
-        loginEmail: '',
-      });
+    this.setState({
+      loginEmail: ""
+    });
   }
 
   onEditProfile() {
-    const {signUpEmail, signUpFirstName, signUpLastName, signUpPassword} = this.state;
-      fetch('/api/account/editprofile?token='+signUpEmail+'&token2='+signUpFirstName+'&token3='+signUpLastName+'&token4='+signUpPassword+'')
-        .then(res => res.json())
-        .then(json6 => {
-          if (json6.success) {
-            this.setState({
-              token,
-              isLoading: false
-            });
-          } else {
-            this.setState({
-              isLoading: false,
-            });
-          }
-        }); 
-}
-  
+    const {
+      signUpEmail,
+      signUpFirstName,
+      signUpLastName,
+      signUpPassword
+    } = this.state;
+    fetch(
+      "/api/account/editprofile?token=" +
+        signUpEmail +
+        "&token2=" +
+        signUpFirstName +
+        "&token3=" +
+        signUpLastName +
+        "&token4=" +
+        signUpPassword +
+        ""
+    )
+      .then(res => res.json())
+      .then(json6 => {
+        if (json6.success) {
+          this.setState({
+            token,
+            isLoading: false
+          });
+        } else {
+          this.setState({
+            isLoading: false
+          });
+        }
+      });
+  }
+
   logout() {
     this.setState({
-      isLoading: true,
+      isLoading: true
     });
-    const obj = getFromStorage('the_main_app');
+    const obj = getFromStorage("the_main_app");
     if (obj && obj.token) {
       const { token } = obj;
       // Verify token
-      fetch('/api/account/logout?token=' + token)
+      fetch("/api/account/logout?token=" + token)
         .then(res => res.json())
         .then(json => {
           if (json.success) {
             this.setState({
-              token: '',
+              token: "",
               isLoading: false
             });
           } else {
             this.setState({
-              isLoading: false,
+              isLoading: false
             });
           }
         });
     } else {
       this.setState({
-        isLoading: false,
+        isLoading: false
       });
     }
   }
@@ -203,18 +231,18 @@ class Login extends Component {
     } = this.state;
 
     if (isLoading) {
-      return (<div><p>Loading...</p></div>);
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
     }
 
     if (!token) {
       return (
         <div>
           <div>
-            {
-              (loginError) ? (
-                <p>{loginError}</p>
-              ) : (null)
-            }
+            {loginError ? <p>{loginError}</p> : null}
             <h1>Log In</h1>
             <input
               name="loginEmail"
@@ -232,17 +260,19 @@ class Login extends Component {
               onChange={this.handleInputChange}
             />
             <br />
-            <button type="button" className="btn btn-dark" onClick={this.onLogin}>Log In</button>
+            <button
+              type="button"
+              className="btn btn-dark"
+              onClick={this.onLogin}
+            >
+              Log In
+            </button>
           </div>
         </div>
       );
     }
 
-    return (
-      <div>
-      </div>
-        
-    );
+    return <div />;
   }
 }
 
