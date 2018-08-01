b22a4fba1923465b0903ebc5654ec4e5a8197248
Jose Carlos Bolaños Marquez <josex670@gmail.com>
2018-08-01 10:22:56 -0700

    Update Catalogue Nutritionist

--- client/app/components/Home/CatalogueNutriologist.js
+++ client/app/components/Home/CatalogueNutriologist.js
@@ -1,11 +1,5 @@
 import React, { Component,Children } from 'react';
 import Modal from 'react-modal';
-
-import {
-  getFromStorage,
-  setInStorage,
-} from '../../utils/storage';
-
 const customStyles = {
   content : {
     top                   : '50%',
@@ -17,7 +11,6 @@ const customStyles = {
     backgroundColor       :'#b9cb34'
   }
 }
-
 class CatalogueNutriologist extends Component {
  
   constructor() {
@@ -27,89 +20,85 @@ class CatalogueNutriologist extends Component {
     this.state = {
       Nutritionists:[],
       isActive:null,
-      activeModal:null,
-      token: ''    
-    };
+      activeModal:null
+
     
+    };
     this.clickHandler =this.clickHandler.bind(this);
     this.hideModal = this.hideModal.bind(this);
-  }
+    //this.toggleModal = this.toggleModal.bind(this);
 
+  }
   clickHandler(e, index) {
     this.setState({ activeModal: index })
-  }
+}
+hideModal() {
+  this.setState({ activeModal: null })
+}
 
-  hideModal() {
-    this.setState({ activeModal: null })
-  }
-  
+
   componentDidMount(){
-    fetch('/api/accounts/nutritionistcatalog', {method:'GET'})
-      .then(res => res.json())
-      .then (json=> {
-          // you set the value of the Nutritionist Contant to that of the JSON you receive
-          this.setState({
-            Nutritionists:json
-          });  
-      });
+  fetch('/api/accounts/nutritionistcatalog', {method:'GET'})
+    .then(res => res.json())
+    .then (json=> {
+        // you set the value of the Nutritionist Contant to that of the JSON you receive
+        this.setState({
+          Nutritionists:json
+        });
+        
+        
+      
+    });
   }
-
-  toggleModal(){
+  toggleModal()
+  {
     this.setState({
       isActive:!this.state.isActive
     });
+
   }
  
-  assignNutrioligistToClient(_id){
-    const obj = getFromStorage('the_main_app');
-    const { token } = obj;
-    console.log(token);
-    fetch('/api/accounts/newPatientRequest', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        Nutritionist_id: _id,
-        Client_id: token,
-        Status:'stand by'
-      }),
-    })
-  }
+render()
+{
+  //This is used to convert the Nutritionist constant from a JSON to an Array
+  var Nutritionists = Array.from(this.state.Nutritionists);
+  //console.log(this.toggleModal);
+ var that=this;
+ console.log(that.clickHandler);
+  return(
+    <div>
+      <h1>Nutriologos</h1>
+      { //using the variable Nutritionist we map the values 
+       Nutritionists.map(function(nutritionist,index){
+        return(
+            // the values are now in nutritionis and the names of the fields
+            //  are the same ast the ones in the JSON
+          <div key={nutritionist._id} className="nutritionist">
+            <div className="card" id="cardstyle" >  
+              <div className="card-body">
+           
+                   <h5 className="card-title"> {nutritionist.FirstName}</h5>
+                    <Modal id= {nutritionist} isOpen={that.state.activeModal == index} onRequestClose={that.hideModal} style={customStyles}>
+                    
+                    {nutritionist.LastName}<br />
+                    {nutritionist.Role}<br />
+                    {nutritionist.Email}<br />
+                    {nutritionist.Phone}<br />
+                    </Modal>
+                    <button  id={nutritionist} onClick={e => that.clickHandler(e,index)}>abrir</button>
+                   
 
-  render(){
-    //This is used to convert the Nutritionist constant from a JSON to an Array
-    var Nutritionists = Array.from(this.state.Nutritionists);
-    var that=this;
-    console.log(that.clickHandler);
-      return(
-        <div>
-          <h1>Nutriologos</h1>
-          { //using the variable Nutritionist we map the values 
-          Nutritionists.map(function(nutritionist,index){
-            return(
-                // the values are now in nutritionist and the names of the fields
-                //  are the same ast the ones in the JSON
-              <div key={nutritionist._id} className="nutritionist">
-                <div className="card" id="cardstyle" >  
-                  <div className="card-body">
-                      <h5 className="card-title"> {nutritionist.FirstName}</h5>
-                        <Modal id= {nutritionist} isOpen={that.state.activeModal == index} onRequestClose={that.hideModal} style={customStyles}>
-                          {nutritionist.LastName}<br />
-                          {nutritionist.Role}<br />
-                          {nutritionist.Email}<br />
-                          {nutritionist.Phone}<br />
-                          <button id={nutritionist} onClick={() => that.assignNutrioligistToClient(nutritionist._id)}>Contact</button>
-                        </Modal>
-                        <button id={nutritionist} onClick={e => that.clickHandler(e,index)}>abrir</button>
-                    </div>
-                  </div>
-                </div>
-                )
+            </div>
+            </div>
+            </div>
+            )
             })}
-        </div>
-      )
+          </div>
+  )
+
   }
 }
+
+  
 
 export default CatalogueNutriologist;
