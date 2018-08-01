e4ef780df512d6aacb8ec97e888ea6af6faa7007
unknown <sergio.jimenez@uabc.edu.mx>
2018-08-01 11:35:10 -0700

    Fixing stuff

--- client/app/components/CorporalAnalysis/CorporalAnalysis.js
+++ client/app/components/CorporalAnalysis/CorporalAnalysis.js
@@ -163,12 +163,12 @@ componentDidMount()
       <div className="col-10 offset-2">
        <p>Fill the client information</p><br />
        
-       <input type="text" name="corpFirstname" placeholder="First Name" value ={corpFirstname} onChange={this.onTextBoxChange}/>
-       <input type="text" name ="corpLastName" placeholder="Last  Name" value ={corpLastName}  onChange={this.onTextBoxChange}/>
+       <input type="tex" name="corpFirstname" placeholder="First Name" value ={corpFirstname} onChange={this.onTextBoxChange}/>
+       <input type="tex" name ="corpLastName" placeholder="Last  Name" value ={corpLastName}  onChange={this.onTextBoxChange}/>
        {/* <input type="text" name="id" placeholder={id} value={id} onChange={this.onTextBoxChange}/><br /> */}
-       <input type="text" name="height" placeholder="Height" value ={height} onChange={this.onTextBoxChange}/>
-       <input type="text" name ="weight" placeholder="Weight" value ={weight} onChange={this.onTextBoxChange}/>
-       <input type="text" name="Age" placeholder="Age" value={Age} onChange={this.onTextBoxChange}/>
+       <input type="tex" name="height" placeholder="Height" value ={height} onChange={this.onTextBoxChange}/>
+       <input type="tex" name ="weight" placeholder="Weight" value ={weight} onChange={this.onTextBoxChange}/>
+       <input type="tex" name="Age" placeholder="Age" value={Age} onChange={this.onTextBoxChange}/>
         </div>
         </div>
         </div>
@@ -176,22 +176,22 @@ componentDidMount()
         <div className="row">
          <div className="col-9 offset-3"> <h3>Composicion Corporal</h3></div>
           <div className="col-10 offset-2">
-          <input type="text"   name =" weight" placeholder="Weight" value ={weight} onChange={this.onTextBoxChange}/><br />
-          <input type="text"  name ="FatFreeBodyMass" placeholder="Fat-Free Body Mass" value ={FatFreeBodyMass} onChange={this.onTextBoxChange}/><br />
-          <input  type="text"  name="FFBMBodyFat" placeholder="FFBM Body Fat" value ={FFBMBodyFat} onChange={this.onTextBoxChange}/><br />
-          <input type="text"  name="LeanSoftTissue" placeholder="Lean Soft Tissue" value ={LeanSoftTissue} onChange={this.onTextBoxChange}/><br />
-          <input type="text"  name ="LSTMineral" placeholder="Mineral" value ={LSTMineral} onChange={this.onTextBoxChange}/><br />
-          <input type="text"  name ="LSTBodyFat" placeholder="Body Fat" value ={LSTBodyFat} onChange={this.onTextBoxChange}/><br />
-          <input type="text"  name="TotalBodyWater" placeholder="Total Body Water" value ={TotalBodyWater} onChange={this.onTextBoxChange}/><br />
-          <input type="text"  name="TBWProtein" placeholder="Protein" value ={TBWProtein} onChange={this.onTextBoxChange}/><br /> 
-          <input type="text"  name ="TBWMineral" placeholder="Mineral" value ={TBWMineral} onChange={this.onTextBoxChange}/><br />  
-          <input type="text"  name="TBWBodyFat" placeholder="Body Fat" value ={TBWBodyFat} onChange={this.onTextBoxChange}/><br />
+          <input type="tex"   name =" weight" placeholder="Weight" value ={weight} onChange={this.onTextBoxChange}/><br />
+          <input type="tex"  name ="FatFreeBodyMass" placeholder="Fat-Free Body Mass" value ={FatFreeBodyMass} onChange={this.onTextBoxChange}/><br />
+          <input  type="tex"  name="FFBMBodyFat" placeholder="FFBM Body Fat" value ={FFBMBodyFat} onChange={this.onTextBoxChange}/><br />
+          <input type="tex"  name="LeanSoftTissue" placeholder="Lean Soft Tissue" value ={LeanSoftTissue} onChange={this.onTextBoxChange}/><br />
+          <input type="tex"  name ="LSTMineral" placeholder="Mineral" value ={LSTMineral} onChange={this.onTextBoxChange}/><br />
+          <input type="tex"  name ="LSTBodyFat" placeholder="Body Fat" value ={LSTBodyFat} onChange={this.onTextBoxChange}/><br />
+          <input type="tex"  name="TotalBodyWater" placeholder="Total Body Water" value ={TotalBodyWater} onChange={this.onTextBoxChange}/><br />
+          <input type="tex"  name="TBWProtein" placeholder="Protein" value ={TBWProtein} onChange={this.onTextBoxChange}/><br /> 
+          <input type="tex"  name ="TBWMineral" placeholder="Mineral" value ={TBWMineral} onChange={this.onTextBoxChange}/><br />  
+          <input type="tex"  name="TBWBodyFat" placeholder="Body Fat" value ={TBWBodyFat} onChange={this.onTextBoxChange}/><br />
             </div>
             <div className="col-9 offset-3"> <h3>Weight Control Evaluation </h3></div> 
             <div className="col-10 offset-2">
-            <input type="text"  name="BodyFat" placeholder="Body Fat" value={BodyFat} onChange={this.onTextBoxChange}/><br />
-            <input type="text"  name="BodyMassIndex" placeholder="Body MassIndex" value={BodyMassIndex} onChange={this.onTextBoxChange}/><br />
-            <input type="text"  name="FatFreeMass" placeholder="Fat Free Mass" value={FatFreeMass} onChange={this.onTextBoxChange}/><br />            
+            <input type="tex"  name="BodyFat" placeholder="Body Fat" value={BodyFat} onChange={this.onTextBoxChange}/><br />
+            <input type="tex"  name="BodyMassIndex" placeholder="Body MassIndex" value={BodyMassIndex} onChange={this.onTextBoxChange}/><br />
+            <input type="tex"  name="FatFreeMass" placeholder="Fat Free Mass" value={FatFreeMass} onChange={this.onTextBoxChange}/><br />            
             </div>
             </div>
 
